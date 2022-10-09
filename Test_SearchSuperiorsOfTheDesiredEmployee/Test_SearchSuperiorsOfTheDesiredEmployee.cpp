ce268ebc8045f70f2e8b79da9f9fcc3787223cd9
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-09 21:33:07 +0300

    Add files via upload

---
+++ Test_SearchSuperiorsOfTheDesiredEmployee/Test_SearchSuperiorsOfTheDesiredEmployee.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Find_boss_NS/tinyxml2.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestSearchSuperiorsOfTheDesiredEmployee
+{
+	TEST_CLASS(TestSearchSuperiorsOfTheDesiredEmployee)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
