80ec86faf959b58336679ac0602648a0574353a2
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-09 21:31:37 +0300

    Add files via upload

---
+++ Test_MatchingIdAndName/Test_MatchingIdAndName.cpp
@@ -0,0 +1,34 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Find_boss_NS/tinyxml2.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestMatchingIdAndName
+{
+	TEST_CLASS(TestMatchingIdAndName)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
+
+/*
+
+string MatchingIdAndName(int id_for_name)
+{
+	for (int i = 0; i < person.size(); i++)
+	{
+		if (person[i].id == id_for_name)
+		{
+			return person[i].Name;
+		}
+	}
+}
+
+
+*/
