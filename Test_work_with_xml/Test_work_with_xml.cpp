0438007184856423df42dc07014dd987f8ec53e2
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-09 22:43:50 +0300

    Add files via upload

---
+++ Test_work_with_xml/Test_work_with_xml.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Find_boss_NS/tinyxml2.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Testworkwithxml
+{
+	TEST_CLASS(Testworkwithxml)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
