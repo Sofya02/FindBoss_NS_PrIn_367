5166a92636caf40cf5ab0e3cc7922fdcbbbf02e6
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-10 20:32:50 +0300

    Add files via upload

---
+++ Test_MatchingIdAndName/Test_MatchingIdAndName.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../FindBoss_NS_PrIn_367/readXML.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestMatchingIdAndName
+{
+	TEST_CLASS(TestMatchingIdAndName)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
