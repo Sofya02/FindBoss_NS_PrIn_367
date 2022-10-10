aa5f98a2bca5fb4213f98b3eaf828b382405a8f6
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-10 20:30:40 +0300

    Add files via upload

---
+++ FindBoss_NS_PrIn_367/Find_boss_NS.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream> //Для работы с клавиатурой и монитором
+#include <fstream> //для работы с файлами 
+#include <string> //для работы getline
+#include <exception> 
+#include "tinyxml2.h"
+#include <vector>
+#include "readXML.h"
+
+using namespace std;
+using namespace tinyxml2;
+
+
+int main(int argc, char* argv[])
+{
+    setlocale(LC_ALL, "Rus");
+    
+    const char* xml_file = "hierarchy_tree.xml";
+
+    WorkingWithXMLFile(xml_file);
+    
+   
+    return 0;
+}
+
+
+
+
+
+
+
