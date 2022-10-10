f6c951f53323c05ea3f6f687d3eeec89175cf462
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-10 20:34:57 +0300

    Add files via upload

---
+++ Test_WorkingWithXMLFile/Test_WorkingWithXMLFile.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../FindBoss_NS_PrIn_367/readXML.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestWorkingWithXMLFile
+{
+	TEST_CLASS(TestWorkingWithXMLFile)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
