ad64984dc090f459d8f67b391b851a5e67ab7c4b
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-10 20:36:26 +0300

    Add files via upload

--- Test_MatchingIdAndName/Test_MatchingIdAndName.cpp
+++ Test_MatchingIdAndName/Test_MatchingIdAndName.cpp
@@ -10,8 +10,21 @@ namespace TestMatchingIdAndName
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
-		{
-		}
+        //Совпадение id найдено
+        TEST_METHOD(IdMatchFound)
+        {
+            int id = 5;
+            string name = "Алефьев Петр Константинович";
+            string exp_Name = MatchingIdAndName(id);
+
+        }
+
+        //Совпадений не найдено
+        TEST_METHOD(InvalidId)
+        {
+            int id = 55;
+            string exp_Name = MatchingIdAndName(id);
+            Assert::IsTrue("Invalid employee id. Perhaps it doesn't exist.");
+        }
 	};
 }
