8d388caf9259098b49133d0a081e8a136eb57157
Sofya02 <106881458+Sofya02@users.noreply.github.com>
2022-10-10 21:09:49 +0300

    Add files via upload

--- Test_WorkingWithXMLFile/Test_WorkingWithXMLFile.cpp
+++ Test_WorkingWithXMLFile/Test_WorkingWithXMLFile.cpp
@@ -12,6 +12,30 @@ namespace TestWorkingWithXMLFile
 		
 		TEST_METHOD(TestMethod1)
 		{
+            const char* file_xml = "";
+            bool res = true;
+            bool exp_res = WorkingWithXMLFile(file_xml);
+            Assert::AreEqual(res, exp_res);
 		}
 	};
 }
+
+
+/*
+
+bool WorkingWithXMLFile(const char* file_xml)
+{
+    XMLDocument doc;
+    doc.LoadFile("hierarchy_tree.xml");
+    if (doc.ErrorID() != 0) {
+        cout << "Invalid input file specified. The file may not exist" << endl;
+        return false;
+    }
+    const char* txt_file = "id.txt";
+    int id_from_txt = GettingIdFromTxtFile(txt_file);
+    XMLElement* node = doc.FirstChildElement()->FirstChildElement("Department");
+    SearchSuperiorsOfTheDesiredEmployee(node, id_from_txt);
+    return true;
+}
+
+*/
